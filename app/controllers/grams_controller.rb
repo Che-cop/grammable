e62c0ff635f4570746e3ac177d8625233da2b4f8
Che-cop <jguymon2002@yahoo.com>
2016-04-18 19:30:27 +0000

    added update feature to grams with tests.

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -5,15 +5,20 @@ before_action :authenticate_user!, only: [:new, :create]
 
   end
 
+
+
   def new
     @gram = Gram.new
   end
 
   def show
     @gram = Gram.find_by_id(params[:id])
-    if @gram.blank?
-      render text: 'Not Found :(', status: :not_found
-    end
+    return render_not_found if @gram.blank?
+  end
+
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
   end
 
 def create
@@ -25,10 +30,28 @@ def create
   end
 end
 
+def update
+  @gram = Gram.find_by_id(params[:id])
+  return render_not_found if @gram.blank?
+
+  @gram.update_attributes(gram_params)
+
+  if @gram.valid?
+    redirect_to root_path
+  else
+    return render :edit, status: :unprocessable_entity
+  end
+
+end
+
 private
 
 def gram_params
   params.require(:gram).permit(:message)
+end
+
+def render_not_found
+    render text: 'Not Found :(', status: :not_found
 end
 
 
