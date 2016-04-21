ef669aab34851c131605f230cc86ca3a5f4a5616
Che-cop <jguymon2002@yahoo.com>
2016-04-21 17:01:03 +0000

    added comments with tests

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,8 +1,10 @@
 class Gram < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
 
   mount_uploader :picture, PictureUploader
 
 validates :message, presence: true
+validates :picture, presence: true
 
 end
